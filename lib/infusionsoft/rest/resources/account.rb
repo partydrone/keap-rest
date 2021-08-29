fe3839862affc03771b4fab721b1435dcfb8ecfe
Andrew Porter <partydrone@icloud.com>
2021-08-29 11:26:32 -0600

    Add update method to account resource

--- lib/infusionsoft/rest/resources/account.rb
+++ lib/infusionsoft/rest/resources/account.rb
@@ -4,6 +4,11 @@ module Infusionsoft
       def info
         Account.new get_request("account/profile").body
       end
+      alias_method :get, :info
+
+      def update(**attributes)
+        Account.new put_request("account/profile", body: attributes).body
+      end
     end
   end
 end
