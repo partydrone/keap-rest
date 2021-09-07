1743c28c0b793e85590b86487703c6703ea06240
Andrew Porter <partydrone@icloud.com>
2021-09-06 18:30:44 -0600

    Reorganize tests to reflect lib structure

--- test/infusionsoft/rest/client/account_profile_test.rb
+++ test/infusionsoft/rest/client/account_profile_test.rb
@@ -1,15 +1,15 @@
 require "test_helper"
 
-class AccountResourceTest < Minitest::Test
-  def test_info
+class AccountProfileTest < Minitest::Test
+  def test_account_profile
     stub = stub_request("account/profile", response: stub_response(fixture: "account/info"))
     client = Infusionsoft::REST::Client.new(access_token: "fake", adapter: :test, stubs: stub)
-    account = client.account.info
+    account = client.account_profile
 
     assert_equal Infusionsoft::REST::Account, account.class
     assert_equal "admin@example.com", account.email
   end
 
-  def test_update
+  def test_update_account_profile
   end
 end
