ede967a59f9542c81fb417f53743c0ab98e38306
Andrew Porter <partydrone@icloud.com>
2021-09-07 04:01:13 -0600

    Remove custom name methods
    I decided it's best to just stick with what the API uses in terms of name attributes for people.

--- lib/infusionsoft/rest/objects/contact.rb
+++ lib/infusionsoft/rest/objects/contact.rb
@@ -1,22 +1,6 @@
 module Infusionsoft
   module REST
     class Contact < Object
-      def first_name
-        given_name
-      end
-
-      def last_name
-        family_name
-      end
-
-      def full_name
-        [given_name, middle_name, family_name].join(" ").squeeze(" ")
-      end
-
-      def name
-        [given_name, family_name].join(" ").squeeze(" ")
-      end
-
       def email
         email_addresses.find { |e| e.field == "EMAIL1" }.email
       end
